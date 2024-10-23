9f90b21acec7feef15f4a0bcf8a05e6a9c388d4b
naol <naolmeseret22@gmail.com>
2024-10-23 10:28:52 +0300

    i finished the functionality of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,150 +1,108 @@
 import { useState } from "react"
-
-const items=[
-{
-  id:0,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-},
-{
-  id:1,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-},
-{
-  id:2,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-},
-{
-  id:3,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-}
-,
-{
-  id:4,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-}
-,
-{
-  id:5,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-}
-,
-{
-  id:6,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-}
-,
-{
-  id:7,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-}
-,
-{
-  id:8,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-}
-,
-{
-  id:9,
-  itemname:"nike shirt",
-  price:12,
-  src:"jordan4.jpg"
-}
-]
-
 function App() {
   const [items,setItems]=useState([{
     id:0,
     itemname:"nike shirt",
     price:12,
-    src:"jordan4.jpg"
+    src:"jordan4.jpg",
+    quantity:0
   },
   {
     id:1,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:15,
+    src:"jordan4.jpg",
+    quantity:0
   },
   {
     id:2,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:19,
+    src:"jordan4.jpg",
+    quantity:0
   },
   {
     id:3,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:11,
+    src:"jordan4.jpg",
+    quantity:0
   }
   ,
   {
     id:4,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:23,
+    src:"jordan4.jpg",
+    quantity:0
   }
   ,
   {
     id:5,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:34,
+    src:"jordan4.jpg",
+    quantity:0
   }
   ,
   {
     id:6,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:45,
+    src:"jordan4.jpg",
+    quantity:0
   }
   ,
   {
     id:7,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:43,
+    src:"jordan4.jpg",
+    quantity:0
   }
   ,
   {
     id:8,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:21,
+    src:"jordan4.jpg",
+    quantity:0
   }
   ,
   {
     id:9,
     itemname:"nike shirt",
-    price:12,
-    src:"jordan4.jpg"
+    price:42,
+    src:"jordan4.jpg",
+    quantity:0
   }])
   const [cartitem,setCartitem]=useState([])
    function handleaddtocart(item){
-    setCartitem([...cartitem,item])
+    setCartitem((prevcart)=>{
+      const itemincart=prevcart.find(cart=>cart.id === item.id);
+      if(itemincart){
+        return prevcart.map(cart=>cart.id ===item.id ? {...cart,quantity:cart.quantity+1}:cart)
+      }
+      else {
+        return [...prevcart ,{...item,quantity:1}]
+      }
+    })
    }
+   const getItemCount = (itemId) => {
+    const itemInCart = cartitem.find(cartItem => cartItem.id === itemId);
+    return itemInCart ? itemInCart.quantity : 0; 
+  };
+
+
    function handledeletecartitem(itemid){
     setCartitem(cartitem.filter((item)=>item.id !== itemid))
    }
+    const calculateTotal = () => {
+    return cartitem.reduce((total, item) => total + (item.price*item.quantity), 0).toFixed(2);
+  };
+
   return (
-    <div>
+    <div className="h-screen">
      <div className="fixed top-0  flex justify-between items-center w-full bg-orange-300 p-2">
       <h1 className="text-sm font-bold sm:text-xl">Brand shoping</h1>
        <div className="flex justify-around w-[20%] sm:w-[9%] items-center">
@@ -163,12 +121,19 @@ function App() {
          </ul>
       </div>
 
-      <div className="max-h-max p-2 ">
-      <ul className="list-none flex flex-wrap divide-y-2">
+      <div className=" p-2   overscroll-y-auto ">
+       {calculateTotal()==0 ? <div className="bg-orange-200 p-2 text-sm text-center font-semibold rounded-md"> please add to cart </div> : <div className="flex justify-between items-center bg-orange-200 p-2 rounded-md">
+        <div className="font-semibold text-lg">Total cost:</div>  <div className="text-lg font-bold">${calculateTotal()}</div>
+       
+        </div>}
+         
+      <ul className="list-none flex flex-wrap divide-y-2 ">
           {
-            cartitem.map((item)=><Cartlist item={item} ondelete={handledeletecartitem} key={item.id} /> )
+            cartitem.map((item)=><Cartlist item={item} ondelete={handledeletecartitem} getItemCount={getItemCount} key={item.id} /> )
           }
+            {/* <li className="absolute bottom-0 right-0 bg-white flex justify-center items-center ">Total price:{calculateTotal()}</li> */}
          </ul>
+      
       </div>
     </div>
     </div>
@@ -184,11 +149,14 @@ function Itemlist({item,handleaddtocart}){
     </li>
   )
 }
-function Cartlist({item,ondelete}){
+function Cartlist({item,ondelete,getItemCount}){
   return <li className="flex justify-between items-center w-full  mt-3">
     <div className="flex flex-row items-center justify-center">
+
     <img src={item.src} alt="" className="h-12 mr-2" />
     <p>${item.price}</p>
+
+    <p className="ml-8">{getItemCount(item.id)}</p>
     </div>
     <button className="bg-red-500 px-3 rounded-md font-medium" onClick={()=>ondelete(item.id)}>Delete</button>
   </li>
